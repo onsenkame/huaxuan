18b4c948e9ea6523daba5df1bd33588803329059
warrenau <onsenkame@gmail.com>
2023-01-11 13:52:05 -0800

    basic art structures added

--- client/components/Home.js
+++ client/components/Home.js
@@ -10,6 +10,54 @@ export const Home = (props) => {
   const { isLoggedIn } = props;
   console.log(props.auth);
 
+  const fanart = [
+    {
+      id: 1,
+      title: 'Exusiai',
+      img: '/img/huaxuanimg/v2/Fanarts/Arknights Exusiai 11x17 (print).jpg'
+    },
+    {
+      id: 2,
+      title: 'Power',
+      img: '/img/huaxuanimg/v2/Fanarts/CSM Power 11.9.22.jpg'
+    },
+    {
+      id: 3,
+      title: 'Reze',
+      img: '/img/huaxuanimg/v2/Fanarts/CSM Reze 11.17.22.jpg'
+    },
+    {
+      id: 4,
+      title: 'Enkidu',
+      img: '/img/huaxuanimg/v2/Fanarts/Enkidu (print).jpg'
+    },
+    {
+      id: 5,
+      title: 'Faputa',
+      img: '/img/huaxuanimg/v2/Fanarts/Made in Abyss Faputa 10.26.22.jpg'
+    },
+    {
+      id: 6,
+      title: 'Nanachi',
+      img: '/img/huaxuanimg/v2/Fanarts/Made in Abyss Nanachi.jpg'
+    },
+    {
+      id: 7,
+      title: 'Merlins',
+      img: '/img/huaxuanimg/v2/Fanarts/Merlins 11x17.jpg'
+    },
+    {
+      id: 8,
+      title: 'Scarlet',
+      img: '/img/huaxuanimg/v2/Fanarts/Scarlet 130k.jpg'
+    },
+    {
+      id: 9,
+      title: 'Exusiai',
+      img: '/img/huaxuanimg/v2/Fanarts/Arknights Exusiai 11x17 (print).jpg'
+    },
+  ]
+
   return (
     <main>
       <section className="container">
@@ -23,20 +71,15 @@ export const Home = (props) => {
         <Container>
           <Row>
             <Col className="fanart-banner">
-            <h1>Welcome to PokeMart!</h1>
-            <p>The best place to buy Pokemon cards online!</p>
+            <h1 className="center">Fanarts</h1>
+            <p className="center">A collection of fanarts</p>
             </Col>
           </Row>
           <Row id="fanarts" className="aligh-items-center">
-            <Col xs={12} md={6} xl={4}>
-            <p>1Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            </Col>
-            <Col>
-            <p>2Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            </Col>
-            <Col>
-            <p>2Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            </Col>
+            {fanart.map((art) => (
+            <Col xs={12} md={6} xl={4} key={art.id}>
+              <img src={art.img} className="img-fluid square" alt="Responsive image"/>
+            </Col>))}
           </Row>
         </Container>
       </section>
